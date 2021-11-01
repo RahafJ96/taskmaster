0f963ebad37388d189151ef4858a8ebb4d77168d
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-11-01 08:28:54 +0200

    lab-26 finished

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.taskmaster;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String title = extras.getString("edit1");
+            String description = extras.getString("edit2");
+
+            TextView textView=(TextView)findViewById(R.id.text1);
+            textView.setText(title +"\n"+description);
+
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                Intent i=new Intent(MainActivity.this,MyTasks.class);
+                startActivity(i);
+                Toast.makeText(this,"Back button pressed!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
