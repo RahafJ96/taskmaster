6b658414ccfb7f21638c4424940075ed8fc90ea7
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-11-06 11:49:34 +0200

    lab-26 sloved

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # taskmaster
+
+### Lab 26 - Beginning TaskMaster
+### The main page
+should have a heading at the top of the page, an image to mock the “my tasks” view, and buttons at the bottom of the page to allow going to the “add tasks” and “all tasks” page.
+
+![main01](screenshots/main-activity-01.PNG)
+![main02](screenshots/main-activity-02.PNG)
+
+### Add a Task
+On the “Add a Task” page, allow users to type in details about a new task, specifically a title and a body. When users click the “submit” button, show a “submitted!” label on the page.
+![addTasks](screenshots/add-task.PNG)
+
+### All Tasks
+The all tasks page should just be an image with a back button; it needs no functionality.
+![allTasks](screenshots/all-tasks.PNG)
--- app/src/main/java/com/example/taskmaster/AllTasks.java
+++ app/src/main/java/com/example/taskmaster/AllTasks.java
@@ -1,27 +1,15 @@
 package com.example.taskmaster;
 
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.taskmaster.Database.DatabaseTask;
-import com.example.taskmaster.Entity.EntityTask;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AllTasks extends AppCompatActivity {
 
@@ -29,73 +17,28 @@ public class AllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String title = extras.getString("edit1");
+            String description = extras.getString("edit2");
+            TextView textView=(TextView)findViewById(R.id.text1);
 
-//        ArrayList<Task> taskData = new ArrayList<Task>();
-//        taskData.add(new Task("study","Study today's lecture","In progress"));
-//        taskData.add(new Task("read","Study today's lecture","In progress"));
-//        taskData.add(new Task("fun","Study today's lecture","In progress"));
-//        taskData.add(new Task("coding","Study today's lecture","In progress"));
-
-        List<EntityTask> taskData = DatabaseTask.getInstance(getApplicationContext()).taskDAO().getAll();
-
-        RecyclerView allStudnetRecyclerView = findViewById(R.id.recTask);
-        allStudnetRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        allStudnetRecyclerView.setAdapter(new TaskAdapter((ArrayList<EntityTask>) taskData));
-
+            textView.setText(title +"\n"+description);
+        }
         ActionBar actionBar = getSupportActionBar();
+
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String title = prefs.getString("title","title");
-        String desc = prefs.getString("description","description");
-        String counter = prefs.getString("counter","total:0");
-
-//        int number = prefs.getInt("number",0);
-
-        TextView textView=(TextView)findViewById(R.id.text1);
-        TextView counterText=(TextView)findViewById(R.id.counter);
-        textView.setText(title +"\n"+desc);
-        counterText.setText(counter);
-    }
-
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        switch (item.getItemId()){
-
+        switch (item.getItemId()) {
             case android.R.id.home:
                 Intent i=new Intent(AllTasks.this,MyTasks.class);
                 startActivity(i);
-                Toast.makeText(this,"Button pressed!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Back button pressed!",Toast.LENGTH_SHORT).show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
-
-    }
-
-    public void intentHelper(View view){
-        Intent intent =new Intent(AllTasks.this,TaskDetails.class);
-
-        Button b = (Button)view;
-        String buttonText = b.getText().toString();
-        System.out.println(buttonText);
-        intent.putExtra("title",buttonText);
-        startActivity(intent);
-    }
-
-    public void ctf(View view) {
-        intentHelper(view);
-    }
-
-    public void code(View view) {
-        intentHelper(view);
-    }
-
-    public void sleep(View view) {
-        intentHelper(view);
     }
 }
